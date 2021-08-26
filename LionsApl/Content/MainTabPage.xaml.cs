8b51cb2bfcdda7f6085d9503127a721a20136a1f
hiromichi-ueda <h_ueda@insat.co.jp>
2021-08-26 13:17:42 +0900

    タブ選択時にナビゲーションのルートに戻る処理追加

--- LionsApl/Content/MainTabPage.xaml.cs
+++ LionsApl/Content/MainTabPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,37 @@ namespace LionsApl.Content
         public MainTabPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnCurrentPageChanged()
+        {
+            base.OnCurrentPageChanged();
+
+            var navigationPage = CurrentPage as NavigationPage;
+
+            // ナビゲーションのルートに戻る
+            navigationPage.PopToRootAsync();
+
+            //DisplayAlert("OnCurrentPageChanged", "カレントページチェンジ", "OK");
+
+            //var currentPage = navigationPage.CurrentPage;
+            //if (currentPage.GetType() == typeof(HomeTop))
+            //{
+            //    DisplayAlert("CurrentPageChanged works correctly", "HomeTop", "ok");
+            //}
+            //else if (currentPage.GetType() == typeof(ClubTop))
+            //{
+            //    DisplayAlert("CurrentPageChanged works correctly", "ClubTop", "ok");
+            //}
+            //else if (currentPage.GetType() == typeof(MatchingTop))
+            //{
+            //    DisplayAlert("CurrentPageChanged works correctly", "MatchingTop", "ok");
+            //}
+            //else if (currentPage.GetType() == typeof(AccountTop))
+            //{
+            //    DisplayAlert("CurrentPageChanged works correctly", "AccountTop", "ok");
+            //}
+
         }
     }
 }
