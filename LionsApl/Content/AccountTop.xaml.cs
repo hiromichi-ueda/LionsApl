1699e56b9068221d20e6a889045ae976d5ba54c0
shoji ikeda <s_ikeda@insat.co.jp>
2021-08-30 17:59:03 +0900

    20210830 edit

--- LionsApl/Content/AccountTop.xaml.cs
+++ LionsApl/Content/AccountTop.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,27 @@ namespace LionsApl.Content
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AccountTop : ContentPage
     {
+        private SQLiteManager _sqlite;                      // SQLiteマネージャークラス
+
+        public ObservableCollection<string> Items { get; set; }
+
         public AccountTop()
         {
             InitializeComponent();
+
+            // SQLite マネージャークラス生成
+            _sqlite = SQLiteManager.GetInstance();
+
+            // A_SETTINGデータ取得
+            _sqlite.SetSetting();
+
+            // タイトル設定
+            Title = _sqlite.Db_A_Setting.CabinetName;
+
+            // アカウント情報データ取得
+
+
+
         }
 
         //---------------------------------------
--- LionsApl/Content/ClubTop.xaml.cs
+++ LionsApl/Content/ClubTop.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,15 +14,75 @@ namespace LionsApl.Content
 
     public partial class ClubTop : ContentPage
     {
-        //private SQLiteManager _sqlite;                      // SQLiteマネージャークラス
-        //private Table.T_SLOGAN _t_slogan;                   // T_SLOGANテーブルクラス
+        private SQLiteManager _sqlite;                      // SQLiteマネージャークラス
+
+        public ObservableCollection<string> Items { get; set; }
 
         public ClubTop()
         {
             InitializeComponent();
 
+            // SQLite マネージャークラス生成
+            _sqlite = SQLiteManager.GetInstance();
+
+            // A_SETTINGデータ取得
+            _sqlite.SetSetting();
+
+            // タイトル設定
+            Title = _sqlite.Db_A_Setting.CabinetName;
 
         }
+
+        //-------------------------------------------
+        /// <summary>
+        /// 年間例会スケジュール一覧画面へ
+        /// </summary>
+        //-------------------------------------------
+        private void Label_ClubSchedule_Taped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ClubScheduleList());
+        }
+
+        //-------------------------------------------
+        /// <summary>
+        /// 理事・委員会一覧画面へ
+        /// </summary>
+        //-------------------------------------------
+        private void Label_Director_Taped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ClubDirectorList());
+        }
+
+        //-------------------------------------------
+        /// <summary>
+        /// 例会プログラム一覧画面へ
+        /// </summary>
+        //-------------------------------------------
+        private void Label_MeetingProgram_Taped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ClubMeetingProgramList());
+        }
+
+        //-------------------------------------------
+        /// <summary>
+        /// 連絡事項（クラブ）一覧画面へ
+        /// </summary>
+        //-------------------------------------------
+        private void Label_Infomation_Taped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ClubInfomationList());
+        }
+
+        //-------------------------------------------
+        /// <summary>
+        /// 会員情報一覧画面へ
+        /// </summary>
+        //-------------------------------------------
+        private void Label_Member_Taped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ClubMemberList());
+        }
+
 
     }
 
